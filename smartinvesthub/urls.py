56b668d4460b207d6cae8b3448ef90bc83159323
brayobiz <briankangogo159@gmai.com>
2025-05-22 14:44:08 +0300

    Fix Gunicorn installation path for Render

--- smartinvesthub/urls.py
+++ smartinvesthub/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 
+def home(request):
+    return HttpResponse("SmartInvestHub Backend is running!")
+    
 urlpatterns = [
+  path('', home, name= 'home'),
     path('admin/', admin.site.urls),
     path('', include('core.urls')), 
 ]
